d816f1516769208459f0d06db9bb3983a1a72764
Tim <tim@Tims-Mac.local>
2019-05-14 18:38:08 +0200

    Fix breadcrumb rotation, add goal breadcrumb

--- AugmentedBreadcrumbs/AugmentedBreadcrumbs/ViewController.swift
+++ AugmentedBreadcrumbs/AugmentedBreadcrumbs/ViewController.swift
@@ -22,7 +22,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     private var goalPosition: CLLocation?
     private var breadcrumbs: [SCNNode] = []
     private var breadcrumbContainer: SCNNode!
+    private var goalNode: SCNNode!
     private var frozen = false
+    
+    
+    //var positions = [CLLocation(latitude: 52.047094, longitude: 12.043262), CLLocation(latitude: 52.047094, longitude: 14.043262), CLLocation(latitude: 50.047094, longitude: 14.043262), CLLocation(latitude: 48.047094, longitude: 14.043262), CLLocation(latitude: 48.047094, longitude: 12.043262), CLLocation(latitude: 48.047094, longitude: 10.043262), CLLocation(latitude: 50.047094, longitude: 10.043262),CLLocation(latitude: 52.047094, longitude: 10.043262)]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,14 +38,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let scene = SCNScene(named: "art.scnassets/ship.scn")!
         sceneView.scene = scene
         
-        
-        breadcrumbContainer = SCNNode(geometry: SCNSphere(radius: 0.01))
+        breadcrumbContainer = SCNNode(geometry: nil)
         breadcrumbContainer.name = "BreadcrumbContainer"
         
         for i in 0...3 {
             let breadcrumbCone = SCNNode(geometry: SCNCone(topRadius: 0, bottomRadius: 0.15, height: 0.3))
-            breadcrumbCone.rotation = SCNVector4(0, 0, 1, -Double.pi/2)
             breadcrumbCone.geometry?.firstMaterial?.diffuse.contents = UIColor.green
+            breadcrumbCone.rotation = SCNVector4(1, 0, 0, Float.pi/2)
             let breadcrumb = SCNNode(geometry: nil)
             breadcrumb.addChildNode(breadcrumbCone)
             breadcrumb.name = "Breadcrumb \(i)"
@@ -50,6 +53,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             breadcrumbs.append(breadcrumb)
             breadcrumbContainer.addChildNode(breadcrumb)
         }
+        
+        goalNode = SCNNode(geometry: SCNCylinder(radius: 0.5, height: 30))
+        breadcrumbContainer.addChildNode(goalNode)
         
         sceneView.scene.rootNode.addChildNode(breadcrumbContainer)
         
@@ -63,11 +69,18 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     func updateBreadcrumbs() {
         if frozen { return }
-        guard let myPos = myPosition, let goalPos = goalPosition else { return }
+        guard let myPos = myPosition, let goalPos = goalPosition, let pov = sceneView.pointOfView else { return }
+        breadcrumbContainer.position = pov.position
         
         let goalMatrix = MatrixCalc.transformMatrix(matrix: matrix_identity_float4x4, myLocation: myPos, newLocation: goalPos)
         let goalVector = SCNVector3(goalMatrix.columns.3.x, 0, goalMatrix.columns.3.z)
         let distance = myPos.distance(from: goalPos)
+        
+        SCNTransaction.begin()
+        SCNTransaction.animationDuration = 0.5
+        SCNTransaction.animationTimingFunction = CAMediaTimingFunction(name: .easeOut)
+        
+        goalNode.position = goalVector
         
         if distance < 10 {
             for breadcrumb in breadcrumbs {
@@ -75,22 +88,27 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                 freeze.layer.borderWidth = 10
                 freeze.layer.borderColor = UIColor.red.cgColor
             }
-            return
+        }
+        else {
+            for breadcrumb in breadcrumbs {
+                breadcrumb.isHidden = false
+                freeze.layer.borderWidth = 0
+                freeze.layer.borderColor = UIColor.green.cgColor
+            }
         }
         
         let relativeDistance = Float(2 / distance)
         let relativeX = relativeDistance * goalVector.x
         let relativeZ = relativeDistance * goalVector.z
+        let angle = atan2(relativeX, relativeZ)
         
         // Update breadcrumbs based on relative distance in x and z (y is equal to camera)
+        
         for breadcrumb in breadcrumbs.enumerated() {
-            breadcrumb.element.position = SCNVector3(relativeX * Float(breadcrumb.offset + 1), 0, relativeZ * Float(breadcrumb.offset + 1))
-            if breadcrumb.element.isHidden {
-                breadcrumb.element.isHidden = false
-                freeze.layer.borderWidth = 0
-                freeze.layer.borderColor = UIColor.green.cgColor
-            }
+            breadcrumb.element.position = SCNVector3(relativeX * Float(breadcrumb.offset + 1), (Float(breadcrumb.offset) * 0.5) - 0.5, relativeZ * Float(breadcrumb.offset + 1))
+            breadcrumb.element.eulerAngles = SCNVector3(0, angle, 0)
         }
+        SCNTransaction.commit()
     }
     
     @IBAction func freeze(_ sender: Any) {
